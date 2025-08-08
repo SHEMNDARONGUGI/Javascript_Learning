e0e7ddf6f6115e234efd5e5b18bce520d5337918
SHEMNDARONGUGI <ndaroshem@gmal.com>
2025-08-08 14:30:28 +0300

    Initial commit

---
+++ DOM/dom.js
@@ -0,0 +1,14 @@
+//By ID
+const elements = document.getElementById("myElement");
+
+//By class Name
+const elements = document.getElementsByClassName("myClass");
+
+//By Tag Name
+const paragraphs = document.getElementsByTagName("p")
+
+// Query Selectors
+const Selectors = document.querySelector("#myElement");
+// id is used for specificity
+
+const elements = document.querySelectorAll(".myClass");
