fd30edb6dbe558613385a4417396109b0cf84e70
SHEMNDARONGUGI <ndaroshem@gmal.com>
2025-08-13 14:21:36 +0300

    Initial commit

---
+++ Event Handling/events.js
@@ -0,0 +1,22 @@
+// element.addEventListener(eventType, handlerFunction);
+
+function clickHandler(){
+    alert("Button Clicked!");
+}
+
+button.addEventListener("click", clickHandler);
+
+// common event in JS
+// 1. Mouse Events:
+// -click, double-click, mousedown, mouseup, mouseover, mouseout, mousemove
+
+// 2. Keyboad Events:
+// -keydown, keyup, keypress
+
+// 3.Form Event
+// -submit, change, focus, blur
+
+//Document/Window event:
+// load, resize, scroll, unload
+
+//event propagation
